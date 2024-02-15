7a1114f550ceb4d597debe7631bb77e4114edd05
Martin Wong <martinwong53@gmail.com>
2024-02-16 00:31:19 +0800

    Initial Commit

---
+++ Legere/LegereApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegereApp.swift
+//  Legere
+//
+//  Created by Martin Wong on 16/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct LegereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
