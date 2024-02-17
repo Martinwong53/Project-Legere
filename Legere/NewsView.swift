cce689ee4c9d374a7c7ca3e6e6cc7b724fc7f79c
Martin Wong <martinwong53@gmail.com>
2024-02-17 23:34:47 +0800

    Added NewsView, ReadingView, SettingView and TabItemView. TabItemView connects all the different Views, also TabItemView is current opening View from initation

---
+++ Legere/NewsView.swift
@@ -0,0 +1,18 @@
+//
+//  NewsView.swift
+//  Legere
+//
+//  Created by Martin Wong on 17/2/2024.
+//
+
+import SwiftUI
+
+struct NewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsView()
+}
---
+++ Legere/ReadingView.swift
@@ -0,0 +1,18 @@
+//
+//  ReadingView.swift
+//  Legere
+//
+//  Created by Martin Wong on 17/2/2024.
+//
+
+import SwiftUI
+
+struct ReadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReadingView()
+}
